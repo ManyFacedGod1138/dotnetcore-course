4e1291e925ea580539319c7cc7096ecb58244be7
ManyFacedGod1138 <szabi1521@yahoo.com>
2019-12-04 19:52:04 +0200

    Az elso ize

---
+++ WebApplication1/Views/Home/Companies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trailers.MVC.Views.Home
+{
+    public class Companies
+    {
+    }
+}
